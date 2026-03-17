7aeb0829720870b4be8212be7247cc74fa720933
Mohan <maheswarimohan781@gmail.com>
2026-03-17 09:14:42 +0530

    removed a typo bug

--- manual_proctoring/renderer/js/dashboard.js
+++ manual_proctoring/renderer/js/dashboard.js
@@ -1,17 +1,11 @@
-window.onload = async function(){
+window.onload = async function () {
+  const res = await fetch('http://localhost:5000/api/student')
+  const student = await res.json()
 
- const res = await fetch("http://localhost:5000/api/student")
-
- const student = await res.json()
-
- document.getElementById("studentName").innerText = student.name
- document.getElementById("examName").innerText = student.exam
-
+  document.getElementById('studentName').innerText = student.name
+  document.getElementById('examName').innerText = student.exam
 }
 
-
-function startExam(){
-
- window.location="exam.html"
-
+function startExam () {
+  window.location = 'exam.html'
 }
